47eda73ee9f3fddfbba01f0e5c4d08d9ad88adb2
Koji Osugi <koji097@gmail.com>
2019-11-03 23:19:11 -0300

    Correção

--- src/main/kotlin/br/com/igdb/Application.kt
+++ src/main/kotlin/br/com/igdb/Application.kt
@@ -4,8 +4,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
 
 @SpringBootApplication
-class IgdbApplication
+class Application
 
 fun main(args: Array<String>) {
-    runApplication<IgdbApplication>(*args)
+    runApplication<Application>(*args)
 }
